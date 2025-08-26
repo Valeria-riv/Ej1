445427e2741a96201d81a1bd4a15f9644312550b
Vale <valeriarivera1132165@correo.itm.edu.co>
2025-08-26 17:18:45 -0500

    Codigo

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Personas persona = new Personas();
+var persona1 = new Personas();
+Estudiantes persona2 = new Estudiantes();
+Personas persona3 = persona2;
+Personas persona4 = (Personas)persona2;
+Personas persona5 = (Estudiantes)persona2;
+
+persona.Id = 1;
+persona.Nombre = "Pepito Perez";
+persona.Estatura = 1.60m;
+persona.Vive = false;
+persona.Fecha = DateTime.Now;
+persona.Estado = new Estados() { Id = 1, Nombre = "Viudo" };
+persona.VideoJuegos = new List<VideoJuegos>();
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 1, Nombre = "GTA" });
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 2, Nombre = "DOOM" });
+
+Console.WriteLine(persona.Nombre);
+Console.WriteLine(persona.Estado.Nombre);
+
+foreach (var elemento in persona.VideoJuegos)
+{
+    Console.WriteLine(elemento.Nombre);
+}
+
+public class Estados
+{
+    public int Id;
+    public string? Nombre;
+
+    public List<Personas>? Personas;
+}
+
+public class VideoJuegos
+{
+    public int Id;
+    public string? Nombre;
+}
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Estatura;
+    public bool Vive;
+    public DateTime Fecha;
+    public Estados? Estado;
+    public List<VideoJuegos>? VideoJuegos;
+}
+
+public interface IEstudiantes
+{
+    bool Matricula();
+}
+
+public interface ISeguros
+{
+    decimal SeguroDeVida(string nombre);
+}
+
+public class Estudiantes : Personas, IEstudiantes, ISeguros
+{
+    public string? Carnet;
+
+    public bool Matricula() 
+    {
+        return true;
+    }
+
+    public decimal SeguroDeVida(string nombre)
+    {
+        return 0.0m;
+    }
+}
